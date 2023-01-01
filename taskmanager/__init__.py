86199bcd00bd486fc01281eb74cba4e1e66e1ef6
ocassidydev <ocassidy@tcd.ie>
2023-01-01 21:17:00 +0000

    Fix spelling mistake

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -10,7 +10,7 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
-    uri = os.encrion.get("DATABASE_URL")
+    uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri
